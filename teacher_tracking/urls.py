2fb7dda8d68c0587e57440e40e96f90bc34cf5c4
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-03-07 10:50:13 -0300

    feat: accessing jwt

--- teacher_tracking/urls.py
+++ teacher_tracking/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from schedule_management.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('schedule_management/user/register', CreateUserView.as_view(), name='register'),
+    path('schedule_management/token', TokenObtainPairView.as_view(), name='get_token'),
+    path('schedule_management/token/refresh', TokenRefreshView.as_view(), name='refresh'),
+    path('schedule_management-auth/', include('rest_framework.urls')),
 ]
