0f83ad9043b955dc6c7bd4560c06353d492b4fc4
RafaelMacharete <rafaelmacharete00@gmail.com>
2025-03-07 13:33:09 -0300

    alterando schedule_management/models.py

--- schedule_management/models.py
+++ schedule_management/models.py
@@ -11,16 +11,17 @@ class User(models.Model):
     def __str__(self):
         return self.user_name
 
-class Schedule(models.Model):
-    class_time = models.CharField(max_length=20)
-    accountable_teacher_name = models.CharField(max_length=100)
-    classroom_enviroment = models.CharField(max_length=30)
+
+class Enviroment(models.Model):
+    enviroment_name = models.CharField(max_length=30)
 
     def __str__(self):
         return self.accountable_teacher_name
 
-class Enviroment(models.Model):
-    enviroment_name = models.CharField(max_length=30)
+class Schedule(models.Model):
+    class_time = models.CharField(max_length=20)
+    accountable_teacher_name = models.CharField(max_length=100)
+    classroom_enviroment = models.ForeignKey(Enviroment, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.enviroment_name
